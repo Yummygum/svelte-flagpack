55372f6d166fa76d6b00fbdb6a3c77e345d64296
Steven Whitton <stevenawhitton@gmail.com>
2021-02-03 11:34:09 +0100

    Fix indentation

--- rollup.config.js
+++ rollup.config.js
@@ -4,13 +4,13 @@ import resolve from 'rollup-plugin-node-resolve';
 const pkg = require('./package.json');
 
 export default {
-        input: 'src/Flag.svelte',
-        output: [
-            { file: pkg.module, 'format': 'es' },
-            { file: pkg.main, 'format': 'umd', name: 'Flag' }
-        ],
-        plugins: [
-            svelte(),
-            resolve()
-        ],
+  input: 'src/Flag.svelte',
+  output: [
+    { file: pkg.module, 'format': 'es' },
+    { file: pkg.main, 'format': 'umd', name: 'Flag' }
+  ],
+  plugins: [
+    svelte(),
+    resolve()
+  ],
 };
