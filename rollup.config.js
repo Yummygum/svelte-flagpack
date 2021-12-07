79e20b3896b24094ea4de4ab6409f567f1ab3b85
yannick1691 <yannick.frisart@outlook.com>
2021-12-07 16:57:46 +0100

    bug: works on fixing bug in Svelte.
    The current implementation of Svelte is not working, this is because we don't include Flag.svelte in the output file.
    
    This leads to an issue as the file is not defined, however the new attempt also doesn't work for the following reasons:
    - Will require users to install SASS into their codebase, which isn't desirable
    - Is seemingly unable to resolve the files that are included in the dist folder.
    - This code is uncompiled, which may or may not be desirable
    
    Relates to: #7

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,7 @@ import { sass } from 'svelte-preprocess-sass';
 import dynamicImportVars from '@rollup/plugin-dynamic-import-vars';
 import minify from 'rollup-plugin-babel-minify'
 import replace from 'rollup-plugin-replace'
+import copy from 'rollup-plugin-copy'
 
 const pkg = require('./package.json');
 
@@ -15,16 +16,35 @@ const env = (process.env.NODE_ENV === prod || process.env.NODE_ENV === dev) ? pr
 
 const plugins = [
   svelte({
-      emitCss: false,
-      preprocess: {
-        style: sass(),
-      },
-    }),
+    emitCss: false,
+    preprocess: {
+      style: sass(),
+    },
+    compilerOptions: {
+
+      // By default, the client-side compiler is used. You
+      // can also use the server-side rendering compiler
+      generate: 'ssr',
+
+      // ensure that extra attributes are added to head
+      // elements for hydration (used with generate: 'ssr')
+      hydratable: true,
+
+      // You can optionally set 'customElement' to 'true' to compile
+      // your components to custom elements (aka web elements)
+      customElement: false
+    }
+  }),
   replace({'process.env.NODE_ENV': JSON.stringify(env)}),
   svg({base64: true}),
   dynamicImportVars(),
   resolve(),
-  common()
+  common(),
+  copy({
+    targets: [
+      { src: 'src/Flag.svelte', dest: 'dist/' }
+    ]
+  })
 ]
 
 export default {
